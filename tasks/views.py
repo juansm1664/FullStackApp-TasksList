eec100bde661e879936ab52651cf16a37f7f4b66
juansm1664 <juansm3007@gmail.com>
2024-04-11 18:19:01 -0500

    django-react

---
+++ tasks/views.py
@@ -0,0 +1,10 @@
+from rest_framework import viewsets
+from .serializer import TaskSerializer
+from .models import Tasks
+
+# Create your views here.
+class TaskView(viewsets.ModelViewSet):
+    serializer_class = TaskSerializer
+    queryset = Tasks.objects.all()
+
+#Crea el crud que se necesita
