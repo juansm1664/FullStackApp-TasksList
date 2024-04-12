086ba809bbd18215463c8a790bda80966b964e52
juansm1664 <juansm3007@gmail.com>
2024-04-11 20:56:35 -0500

    practicando python con react

--- crud_api/settings.py
+++ crud_api/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'rest_framework',
+    'coreapi',
     'tasks'
     
 ]
@@ -128,3 +129,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = []
+
+
+REST_FRAMEWORK = {
+    ...: ...,
+    "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema",
+}
--- tasks/urls.py
+++ tasks/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from rest_framework.documentation import include_docs_urls
 from rest_framework import routers
 from tasks import views
 
@@ -10,7 +11,8 @@ router.register(r'tasks', views.TaskView,'tasks')
 
 
 urlpatterns = [
-    path("api/v1/", include(router.urls))
+    path("api/v1/", include(router.urls)),
+    path('docs/', include_docs_urls(title="Tasks_Api"))
 ]
 
 #########
